8da032ea274f114954b8d4d0324630e06e0c4ae8
Juan Corredor <ca_juan@javeriana.edu.co>
2022-08-10 15:41:09 -0500

    Agregada la funcion de crear contacto

--- main.py
+++ main.py
@@ -15,8 +15,19 @@ menu_sel = 0
 menu()
 while menu_sel != 6:
 	menu_sel = input("Seleccione una opcion entre 1 y 6")
+	#Guardar contacto
 	if menu_sel == 1:
-
+		name = input("Nombre:")
+		nombres.append(name)
+		num = input("Numero:")
+		numero.append(num)
+		like = input("Te agrada? (si/no)")
+		if like == "si":
+			estado.append(True)
+		elif like == "no"
+			estado.append(False)
+		else
+			print("Error")
 	elif  menu_sel == 2:
 
 	elif menu_sel == 3:
