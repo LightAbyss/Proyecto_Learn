1b0c0a7f3573a2734fae141b80dd906609566b8c
Juan Corredor <ca_juan@javeriana.edu.co>
2022-08-10 15:33:51 -0500

    Declaracion de variables, inclusion del while principal y selector de opciones

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+#funcion para imprimir el menu
 def menu():
 	print("1. Crear contacto")
 	pritnt("2. Lista blanca")
 	print("3. Lista negra")
 	print("4. Borrar contacto")
 	print("5. Buscar contacto")
+	print("6. Salir")
+#Declaracion de variables
+nombres = []
+numero = []
+estado = []
+menu_sel = 0
+#Inicio del main
+menu()
+while menu_sel != 6:
+	menu_sel = input("Seleccione una opcion entre 1 y 6")
+	if menu_sel == 1:
 
+	elif  menu_sel == 2:
 
+	elif menu_sel == 3:
+
+	elif menu_sel == 4:
+
+	elif menu_sel == 5:
+
+	elif menu_sel != 6:
+		menu()
