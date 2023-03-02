4393903038dfc9c1fc7d4aca07a0885c95e5ad2e
Juan Corredor Castro (Nokia) <juan.corredor_castro@nokia.com>
2023-03-02 16:07:36 -0500

    Creada version en C. Faltan las funciones

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void menu();
+
+int main(){
+    menu();
+
+}
+
+void menu(){
+    printf("1. Crear contacto\n");
+	printf("2. Lista blanca\n");
+	printf("3. Lista negra\n");
+	printf("4. Borrar contacto\n");
+	printf("5. Buscar contacto\n");
+	printf("6. Salir\n");
+
+}
