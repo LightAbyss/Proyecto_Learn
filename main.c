0106b6797bcdb45150d8f5cbff4586999e183ea5
Juan Corredor Castro (Nokia) <juan.corredor_castro@nokia.com>
2023-03-02 16:11:01 -0500

    Creada version en C. Faltan las funciones

--- main.c
+++ main.c
@@ -1,11 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//Definicion de estructura
+typedef struct contact{
+    char nombre[20];
+    int numero;
+    char direccion[40];
+    char like;
+} contact;
+//Declaracion de funciones
 void menu();
+void crear();
+void blanco();
+void negro();
+void borrar();
+void buscar();
+//Declaracion de variables
+int opcion;
+contact lista[100];
+int posi;
 
 int main(){
-    menu();
-
+    opcion = 0;
+    posi = 0;
+    while(opcion != 6){
+        menu();
+        scanf("%d",&opcion);
+        switch (opcion){
+        case 1:
+            crear();
+            break;
+        case 2:
+            blanco();
+            break;
+        case 3:
+            negro();
+            break;
+        case 4:
+            borrar();
+            break;
+        case 5:
+            buscar();
+            break;
+        default:
+            printf("Error, intente otra opcion");
+            break;
+        }
+    }
 }
 
 void menu(){
@@ -15,5 +56,9 @@ void menu(){
 	printf("4. Borrar contacto\n");
 	printf("5. Buscar contacto\n");
 	printf("6. Salir\n");
+}
+
+void crear(){
+
 
 }
