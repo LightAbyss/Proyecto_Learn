cbff81d28f29659b14be062b39dc25b59a06c2a3
Juan Corredor Castro (Nokia) <juan.corredor_castro@nokia.com>
2023-03-06 15:03:43 -0500

    Declaracion de variables - inicio de main

--- Agenda/main.rs
+++ Agenda/main.rs
@@ -1,4 +1,4 @@
-std::io;
+use std::io;
 
 struct contact{
     nombre: String,
@@ -7,9 +7,35 @@ struct contact{
     like: bool,
 }
 
+let mut opcion:u8;
+let mut lista:contact;
+let mut temp:contact;
+let mut posi:u16;
+let mut positmp:u16;
+
+
 fn main(){
+    opcion = 0;
+    poi = 0;
+    positmp = 0;
 
+    while(opcion != 6){
+        menu();
 
+        io::stdin()
+            .read_line(&mut opcion)
+            .expect("Error de lectura");
+    }
+
+}
+
+fn menu(){
+    println!("1. Crear contacto");
+	println!("2. Lista blanca");
+	println!("3. Lista negra");
+	println!("4. Borrar contacto");
+	println!("5. Buscar contacto");
+	println!("6. Salir");
 }
 
 fn crear(){
