e5ccbe7053bf7239ead1ed6238d257dac49af5ff
Juan Corredor Castro (Nokia) <juan.corredor_castro@nokia.com>
2023-03-06 14:37:47 -0500

    Inicio de archivo rust

---
+++ Agenda/main.rs
@@ -0,0 +1,33 @@
+std::io;
+
+struct contact{
+    nombre: String,
+    numero: u16,
+    direccion: String,
+    like: bool,
+}
+
+fn main(){
+
+
+}
+
+fn crear(){
+
+}
+
+fn blanca(){
+
+}
+
+fn negra(){
+
+}
+
+fn buscar(){
+
+}
+
+fn borrar(){
+
+}
